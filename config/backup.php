283cd69ecdf06c9ef339eb89ca4bdfc83ccd5a72
François Lévesque <francois@witify.io>
2019-05-25 16:30:04 -0400

    Follow Symlinks

--- config/backup.php
+++ config/backup.php
@@ -34,7 +34,7 @@ return [
                 /*
                  * Determines if symlinks should be followed.
                  */
-                'followLinks' => false,
+                'followLinks' => true,
             ],
 
             /*
